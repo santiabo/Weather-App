07fc40f234342498831f922ade0f084c741b3891
Santiago Aguirre <santiabo@gmail.com>
2021-02-02 12:17:48 -0300

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,7 +7,7 @@ import About from '../components/About.jsx';
 import Ciudad from '../components/Cuidad.jsx';
 import { Route } from 'react-router-dom';
 require('dotenv').config();
-const { key } = process.env;
+const { API_KEY } = process.env;
 
 function App() {
   const [cities, setCities] = useState([]);
@@ -16,7 +16,7 @@ function App() {
   }
   function onSearch(ciudad) {
     //Llamado a la API del clima
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${key}&units=metric`)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${API_KEY}&units=metric`)
       .then(r => r.json())
       .then((recurso) => {
         if(recurso.main !== undefined){
